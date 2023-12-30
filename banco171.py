a5529aa43ae04438623226b712dcc9ddf06f22cd
AlexSilva91 <alexsilvaalves17@outlook.com>
2023-12-29 22:22:07 -0300

    config inicial

---
+++ banco171.py
@@ -0,0 +1,59 @@
+from conta import Conta
+
+def exibir_menu():
+    print('*********BANCO-171***********')
+    print('* 0 - Sair')
+    print('* 1 - Abrir conta')
+    print('* 2 - Depósito')
+    print('* 3 - TED')
+    print('* 4 - Listar contas')
+    print('*********BANCO-171***********')
+
+def cadastrar_conta():
+    print('Abrindo conta....\n*******************************')
+    cpf=int(input('Digite o CPF: '))
+    nome=input('Digite seu nome: ')
+    senha=int(input('Defina sua senha: '))
+    num_conta=int(input('Número da conta: '))
+    saldo=float(input('Valor do depósito inicial: '))
+    status='ativo'
+    
+    conta=Conta(cpf,nome,senha,num_conta,saldo,status)
+
+    print('*******************************')
+    print('\nConta aberta com sucesso!')
+    print('*******************************')
+
+def processar_escolha(escolha):
+    if escolha == 0:
+        print('\nSaindo...')
+    elif escolha==1:
+        cadastrar_conta()
+    elif escolha==2:
+        cont=int(input('\nConta: '))
+        deposit=float(input('\nValor de depósito: '))
+        if Conta.atualizar_saldo_por_conta(cont, deposit):
+            print('\n******Depósito realizado com sucesso!******')
+    elif escolha==3:
+        cont_origem=int(input('\nConta de Origem: '))
+        cont_destino=int(input('\nConta de Destino: '))
+        valor=float(input('\nValor a transferir: '))
+        if Conta.tranfere_valor_entre_contas(cont_origem,cont_destino,valor):
+            print('00000000000000000000000000000\nTransferência realizada com sucesso!')
+        else:
+            print('00000000000000000000000000000\nImpossível realizar transferência!')
+    elif escolha==4:
+        Conta.listar_contas()
+    else:
+        print('\nOpção inválida! Tente novamente.\n')
+
+def main():
+    while True:
+        exibir_menu()
+        escolha = int(input('Qual operação deseja realizar: '))
+        processar_escolha(escolha)
+        if escolha==0:
+            break
+
+if __name__=="__main__":
+    main()
---
+++ conta.py
@@ -0,0 +1,45 @@
+class Conta:
+    contas_cadastradas =[]
+    def __init__(self,cpf,nome,senha,num_conta,saldo,status):
+        self.cpf=cpf
+        self.nome=nome
+        self.senha=senha
+        self.num_conta=num_conta
+        self.saldo=saldo
+        self.status=status
+        Conta.contas_cadastradas.append(self)
+
+    def apresentar(self):
+        return '\nNome: {}\nCPF: {}\nConta: {}\nSaldo: {}\nStatus: {}'.format(self.nome, self.cpf,self.num_conta,self.saldo,self.status)
+
+    @classmethod
+    def listar_contas(cls):
+        for conta in cls.contas_cadastradas:
+            print(conta.apresentar())
+
+    @classmethod
+    def buscar_por_conta(cls, num_conta):
+        for conta in cls.contas_cadastradas:
+            if conta.num_conta == num_conta:
+                return conta
+        return None
+    
+    @classmethod
+    def atualizar_saldo_por_conta(cls, conta,deposit):
+        c = cls.buscar_por_conta(conta)
+        if c:
+            c.saldo += deposit
+            return True  # Atualização bem-sucedida
+        return False  # Pessoa não encontrada
+    
+    @classmethod
+    def tranfere_valor_entre_contas(cls, origem, destino, valor):
+        conta_origem=cls.buscar_por_conta(origem)
+        conta_destino=cls.buscar_por_conta(destino)
+        if conta_origem and conta_destino:
+            if conta_origem.saldo>=valor:
+                conta_destino.saldo+=valor
+                conta_origem.saldo-=valor
+                return True
+            else:
+                return False
